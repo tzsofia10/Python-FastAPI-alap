6686fb1f5db2da2978a28ff4df07e5d153631d74
Csizmadia Márk <airamek@teamorange.hu>
2025-01-07 19:35:52 +0100

    fix: move user fields

--- app/models/user.py
+++ app/models/user.py
@@ -4,15 +4,17 @@ from datetime import date
 
 class UserBase(SQLModel):
     username: str = Field(default=None, max_length=64)
-    high_score: int
-    json_save: str
    
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     password_hash: str = Field(default=None, max_length=256)
+    high_score: int
+    json_save: str
    
 class UserPublicSmall(UserBase):
     id: Optional[int]
+    high_score: int
+    json_save: str
 
 class UserPublic(UserPublicSmall):
     pass
